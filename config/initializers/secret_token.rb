900895dcb44a7130362daf901742dbe084650799
ping <pingpku@163.com>
2013-07-16 23:39:50 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c3659fe79a599baa1e9c33ace901bb4f01848b5002e1a4fb64b6916055c7c158866d6a831915356d4a8a6f7c501bfa7d5ca7f39638b8d1b1bc0bc1d8248a76ef'
